8ca8fdd512a27c603baeb79a2ff1bfe6252867c7
Annie Phan <anniephan.atp@gmail.com>
2019-03-08 08:32:58 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,8 @@
 def alphabetize(arr)
   # code here
-  arr.sort_by do |word|
-    word.split(" ").collect do |letter|
-      arr.index(letter)
+  arr.sort_by do |sentence|
+    sentence.split(" ").collect do |char|
+      ESPERANTO_ALPHABET.index(char)
     end
   end
 end
