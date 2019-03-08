f71ab73101fb1de241e3b1d100d866ffe3f1b2a0
Annie Phan <anniephan.atp@gmail.com>
2019-03-08 08:33:27 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,7 @@
-def alphabetize(arr)
-  # code here
-  arr.sort_by do |sentence|
-    sentence.split(" ").collect do |char|
-      ESPERANTO_ALPHABET.index(char)
+def alphabetize(sentences_array)
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
     end
   end
 end
